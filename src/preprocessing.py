199d765ae362716935eaf72f729c2f139975a9b9
exoks <exoks.team@gmail.com>
2025-07-26 12:33:18 +0100

    data cleaning: remove duplicates > Done

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,7 +5,7 @@
 #  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀
 #  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
 #  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2025/07/24 13:45:35 by oezzaou
-#  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/07/25 00:06:40 by oezzaou
+#  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/07/26 12:32:24 by oezzaou
 #  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀
 #  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀
 #  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪
@@ -13,10 +13,41 @@
 #  ⠀⠀⠀⠀⠀⠘⠄⣀⡀⠸⠓⠀⠀⠀⠠⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
 # ===[ Imports: ]==============================================================
+from src.utils.logger import getLogger
 import pandas as pd
+
+
+# ===[ remove_duplicates: ]====================================================
+def remove_duplicates(data: pd.DataFrame) -> pd.DataFrame:
+    '''
+    It removes samples (or cases) that are duplicated from `data` set
+    '''
+    logger = getLogger(__name__)
+    logger.info("Droping duplicates from raw data ...")
+    no_dup_data = data.drop_duplicates()
+    if no_dup_data.shape[0] < data.shape[0]:
+        logger.debug(f"{data.shape[0] - no_dup_data.shape[0]}: rows dropped")
+    else:
+        logger.debug("No duplicated rows found")
+    return no_dup_data
 
 
 # ===[ handle_missing: ]=======================================================
 def handle_missing(data: pd.DataFrame) -> pd.DataFrame:
-    # Define how to handle missings (drop missings or imputation)
+    # Define how to handle missings (Drop missings or Imputation)
     return data
+
+
+# ===[ fix_inconsistent_formats: ]=============================================
+def fix_inconsistent_formats() -> pd.DataFrame:
+    pass
+
+
+# ===[ clean_data: ]===========================================================
+def clean_data(data: pd.DataFrame):
+    # 1. Remove Duplicates
+    no_dup_data = remove_duplicates(data)
+    # 2. Handle Missings
+    no_missings_data = handle_missing(no_dup_data)
+    # 3. Fix Inconsistent formats
+    return no_missings_data
